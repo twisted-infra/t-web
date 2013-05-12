f437cb5ea66675277e743328658ece8f4b8779c0
tom.prince@ualberta.net <>
2013-05-12 07:18:18 -0700

    Use absolute paths and restart on update and use braid.archive.

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,12 @@
 Support for wwww service installation and management.
 """
 
-from fabric.api import run, settings, env, cd, put, abort, sudo
+from fabric.api import run, settings, env, put, abort, sudo
 from fabric.contrib import files
 
-from braid import authbind, bazaar, cron
+from braid import authbind, bazaar, cron, archive
 from braid.twisted import service
 from braid.debian import equivs
-from braid.utils import tempfile
 
 # TODO: Move these somewhere else and make them easily extendable
 from braid import config
@@ -35,16 +34,16 @@ class TwistedWeb(service.Service):
         equivs.installEquiv(self.serviceName, 'httpd')
 
         with settings(user=self.serviceUser):
-            run('ln -nsf {}/start {}/start'.format(self.configDir, self.binDir))
+            run('/bin/ln -nsf {}/start {}/start'.format(self.configDir, self.binDir))
             self.task_update()
             cron.install(self.serviceUser, '{}/crontab'.format(self.configDir))
 
-            run('mkdir -p ~/data')
+            run('/bin/mkdir -p ~/data')
             if env.get('installPrivateData'): 
                 self.task_installSSLKeys() 
-                run('touch {}/production'.format(self.configDir))
+                run('/usr/bin/touch {}/production'.format(self.configDir))
             else:
-                run('rm -f {}/production'.format(self.configDir))
+                run('/bin/rm -f {}/production'.format(self.configDir))
 
 
     def task_installSSLKeys(self, key=None, cert=None):
@@ -74,27 +73,27 @@ class TwistedWeb(service.Service):
         with settings(user=self.serviceUser):
             # TODO: This is a temp location for testing
             bazaar.branch('lp:~tom.prince/twisted-website/twisted-website-braided', self.configDir)
-            # TODO restart
+            self.task_restart()
 
 
     def task_dump(self, dump):
         """
         Dump non-versioned resources.
         """
-        with settings(user=self.serviceUser), \
-             tempfile(saveTo=dump) as tar, \
-             cd('~/data'):
-            run('/usr/bin/tar -c -j -f {} .'.format(tar))
+        with settings(user=self.serviceUser):
+            archive.dump({
+                'data': '~/data',
+                }, dump)
 
 
     def task_restore(self, dump):
         """
         Resotre non-versioned resources.
         """
-        with settings(user=self.serviceUser), \
-             tempfile(saveTo=dump) as tar, \
-             cd('~/data'):
-            run('/usr/bin/tar -x -j -f {} .'.format(tar))
+        with settings(user=self.serviceUser):
+            archive.restore({
+                'data': '~/data',
+                }, dump)
 
 
 
