01abc8eaeb95eb433210325e7314552a02419bf9
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2013-08-31 08:23:54 -0400

    Add some memory debugging for the PyPy runtime

---
+++ twisted-web/local.d/pypymemdbg.py
@@ -0,0 +1,14 @@
+import gc, os
+
+from twisted.application.internet import TimerService
+
+class MemoryDebugService(TimerService):
+    counter = 0
+
+    def __init__(self):
+        TimerService.__init__(self, 60 * 60, self._report)
+
+
+    def _report(self):
+        gc.dump_rpy_heap(b"%d-%d.rpy_heap" % (os.getpid(), self.counter))
+        self.counter += 1
